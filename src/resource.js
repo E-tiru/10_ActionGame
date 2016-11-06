31ccab7baf63d66e2735b7ebd53a31eae56bec1e
Masaki Isono <isonoetiru@gmail.com>
2016-11-07 05:42:41 +0900

    titleの実装

--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,36 @@
 var res = {
-   bat_frames: "res/bat_frames.png",
+ background_back : "res/background_back.png",
+ background_front : "res/background_front.png",
+ background_light : "res/background_light_shafts.png",
+ bat_frames : "res/bat_frames.png",
+ block : "res/block48X48.png",
+ coins_frames : "res/coins.png",
+ curtain : "res/curtain.png",
+ curtain2 : "res/curtain2.png",
+ dead_sword : "res/dead_sword.png",
+ glass_frames : "res/glass_frames.png",
+ item_health : "res/item_health.png",
+ particle_dot : "res/particle_dot.png",
+ player_sheet : "res/sir_awesome_frames.png",
+ slime_green_frames : "res/slime_green_frames.png",
+ slime_green_plist : "res/slimegreen.plist",
+ slime_red_frames : "res/slime_red_frames.png",
+ slime_red_plist : "res/slimered.plist",
+ slime_yellow_plist : "res/slimeyellow.plist",
+ sparkle : "res/sparkle_frames.png",
+ title : "res/title.png",
+ ui_pauge : "res/ui_gauge_fill.png",
+ ui_panels : "res/ui_panels.png",
+ zombie_frames : "res/zombie_frames.png",
+ leftBtn : "res/leftbutton.png",
+ rightBtn : "res/rightbutton.png",
+ jumpBtn : "res/jumpbutton.png",
+ player_plist : "res/player.plist",
+ zombie_plist : "res/zombie.plist"
+
+
+
+
 
 
 };
